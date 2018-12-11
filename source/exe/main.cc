c7f6f90e274e19a204908848dc8b9b3500940e52
Sam Smith <sesmith177@gmail.com>
2018-12-11 09:39:18 -0500

    wip
    Signed-off-by: Natalie Arellano <narellano@pivotal.io>
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ source/exe/main.cc
@@ -0,0 +1,34 @@
+#include "exe/main_common.h"
+#include "exe/platform_setup.h"
+
+
+// NOLINT(namespace-envoy)
+
+/**
+ * Basic Site-Specific main()
+ *
+ * This should be used to do setup tasks specific to a particular site's
+ * deployment such as initializing signal handling. It calls main_common
+ * after setting up command line options.
+ */
+int main(int argc, char** argv) {
+  Envoy::PlatformSetup setup(argc, argv);
+  std::unique_ptr<Envoy::MainCommon> main_common;
+
+  // Initialize the server's main context under a try/catch loop and simply return EXIT_FAILURE
+  // as needed. Whatever code in the initialization path that fails is expected to log an error
+  // message so the user can diagnose.
+  try {
+    main_common = std::make_unique<Envoy::MainCommon>(argc, argv, setup);
+  } catch (const Envoy::NoServingException& e) {
+    return EXIT_SUCCESS;
+  } catch (const Envoy::MalformedArgvException& e) {
+    return EXIT_FAILURE;
+  } catch (const Envoy::EnvoyException& e) {
+    return EXIT_FAILURE;
+  }
+
+  // Run the server listener loop outside try/catch blocks, so that unexpected exceptions
+  // show up as a core-dumps for easier diagnostis.
+  return main_common->run() ? EXIT_SUCCESS : EXIT_FAILURE;
+}
