d33eb47492cf727127ecbfc263bafdcb65cfdebd
Yechiel Kalmenson <ykalmenson@pivotal.io>
2019-06-20 17:05:07 -0400

    Fixed a few more compilation errors
    * Replaced all `SOCKET_HANDLE`s with `Network::IoHandle`s
    * Fixed the merge conflict in `source/common/buffer/buffer_impl.cc`
      regarding the `read` and the `write` functions (used the linux
      implementation)
    
    Signed-off-by: Yael Harel <yharel@pivotal.io>

--- source/common/network/io_socket_handle_impl.h
+++ source/common/network/io_socket_handle_impl.h
@@ -41,11 +41,11 @@ private:
 #ifdef WIN32
   SOCKET socket_descriptor_;
 #else
-  // Converts a SysCallSizeResult to IoCallUint64Result.
-  Api::IoCallUint64Result sysCallResultToIoCallResult(const Api::SysCallSizeResult& result);
-
   int fd_;
 #endif
+
+  // Converts a SysCallSizeResult to IoCallUint64Result.
+  Api::IoCallUint64Result sysCallResultToIoCallResult(const Api::SysCallSizeResult& result);
 };
 
 } // namespace Network
