381968b78d0cb563f96ec48a4db5cf97a01d7e6d
William A Rowe Jr <wrowe@pivotal.io>
2020-01-17 10:33:26 -0500

    Coallesce directory management to Envoy::TestEnvironment
    - Drop std::[*]filesystem logic
    - Implement/tests for new Envoy::Filesystem::splitFileName
    - Remove unneeded TestEnvironment::createParentPath
    - Move/rename Envoy::TestUtility::createDirectory to Envoy::TestEnvironment::createPath
    - Move/rename Envoy::TestUtility::removeDirectory to Envoy::TestEnvironment::removePath
    - Move Envoy::TestUtility::renameFile to Envoy::TestEnvironment
    - Move Envoy::TestUtility::createSymlink to Envoy::TestEnvironment
    - Move AtomicFileUpdater from test_common utility to environment
    - Simplify Envoy::Filesystem::Watcher implementations using Filesystem
    - Clean up watcher_impl flavors for splitFileName API
    - Implement/new tests for Envoy::Filesystem::illegalPath for win32
    - Implement watcher_impl for win32 [Initial development by Sam Smith]
    - Ensure top level test tempdir exists when requested
    
    Co-authored-by: Sunjay Bhatia <sbhatia@pivotal.io>
    Co-authored-by: William A Rowe Jr <wrowe@pivotal.io>
    Co-authored-by: Sam Smith <sesmith177@gmail.com>
    
    Signed-off-by: William A Rowe Jr <wrowe@pivotal.io>

--- test/mocks/filesystem/mocks.h
+++ test/mocks/filesystem/mocks.h
@@ -52,6 +52,7 @@ public:
   MOCK_METHOD(bool, directoryExists, (const std::string&));
   MOCK_METHOD(ssize_t, fileSize, (const std::string&));
   MOCK_METHOD(std::string, fileReadToEnd, (const std::string&));
+  MOCK_METHOD(PathSplitResult, splitFileName, (absl::string_view));
   MOCK_METHOD(bool, illegalPath, (const std::string&));
 };
 
